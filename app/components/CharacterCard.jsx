9c9c0509003646c5f638a532358556d1026dd9c8
Berktug Berke Ates <berktugates@gmail.com>
2023-12-10 06:45:30 +0300

    home page and locations page design was made

---
+++ app/components/CharacterCard.jsx
@@ -0,0 +1,21 @@
+const Card = ({name,status,gender,image}) => {
+    return (
+      <>
+          <div className="rounded-lg border-2 border-blue-500 shadow-2xl mt-4">
+              <a className="flex justify-center rounded-lg" href="#">
+                  <img className="mt-2 border border-black" src={image} width={150} alt={name}/>
+              </a>
+              <div className="p-5 flex flex-col">
+                  <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">{name}</h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">Status : {status}</p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">Gender : {gender}</p>
+                  <button type="button" className="text-white bg-button bg-green-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Review</button>
+              </div>
+          </div>
+      </>
+    )
+  }
+  
+  export default Card
---
+++ app/home/page.js
@@ -0,0 +1,39 @@
+"use client"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import CharacterCard from "../components/CharacterCard"
+import Navbar from "../components/Navbar"
+
+export default function Home() {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(()=>{
+    axios
+    .get('https://rickandmortyapi.com/api/character')
+    .then((response)=>{
+      setCharacters(response.data.results)
+    })
+    .catch((error)=>{
+      console.log(error,"error");
+    })
+  },[])
+
+  return (
+    <>
+    <Navbar/>
+      <div className="flex justify-center items-center mt-auto">
+          <div className="grid grid-cols-4 gap-4">
+            {characters.map((character,index)=>{
+              return <CharacterCard
+                key={index}
+                name = {character.name}
+                status = {character.status}
+                gender = {character.gender}
+                image = {character.image}
+              />
+            })}
+          </div>
+      </div>
+    </>
+  )
+}

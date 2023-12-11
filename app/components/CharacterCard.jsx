44c97c27e27e2f31f7bd816a1ac4be6b7407d659
Berktug Berke Ates <berktugates@gmail.com>
2023-12-11 03:41:27 +0300

    update : CharacterCard.jsx component

--- app/components/CharacterCard.jsx
+++ app/components/CharacterCard.jsx
@@ -1,4 +1,4 @@
-const Card = ({name,status,gender,image}) => {
+const Card = ({name,status,gender,image,counter}) => {
     return (
       <>
           <div className="rounded-lg border-2 border-blue-500 shadow-2xl mt-4">
@@ -11,7 +11,7 @@ const Card = ({name,status,gender,image}) => {
                   </a>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">Status : {status}</p>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">Gender : {gender}</p>
-                  <button type="button" className="text-white bg-button bg-green-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Review</button>
+                  <button type="button" className="text-white bg-green-500 focus:ring-4 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">{counter}</button>
               </div>
           </div>
       </>

d2532832ec3da5047ae7b4ebdc210fb77bd768bc
Berktug Berke Ates <berktugates@gmail.com>
2023-12-12 02:58:08 +0300

    fix : syntax errors

--- app/loading.js
+++ app/loading.js
@@ -3,7 +3,7 @@ import Loading from "./components/Loading"
 export default function loading() {
   return (
     <>
-        <div class="flex h-screen items-center justify-center">                
+        <div className="flex h-screen items-center justify-center">                
                 <Loading/>
         </div>
     </>
